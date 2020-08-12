d8a9c8d27804de37161518027921380217b36015
PouRiaBG <pooria.bonyadgozar@gmail.com>
2020-08-12 12:35:59 +0430

    Added some help comment

--- src/services/fetchWeather.ts
+++ src/services/fetchWeather.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 // import config file that have api key, you can get your api can from openweathermap.org
+// sample url : http://api.openweathermap.org/data/2.5/weather?units=metric&appid=API_KEY&q=CITY_NAME
 import {config} from '../config'
 export const fetchWeather = async (cityName : string)=>{
     const URL = `${config.key}&q=${cityName}`
